c1848f0208d5ef8a9d97d561bfc9eac55d786b84
Acasune <tttt4aaaa@gmail.com>
2021-07-17 15:04:55 +0900

    ch4:multi process test

---
+++ ch4/fibonacci.js
@@ -0,0 +1,5 @@
+﻿'use strict'
+
+module.exports = function fibonacci(n) {
+  return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2)
+}
---
+++ ch4/multi-process.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+const { fork, setupMaster } = require('cluster')
+
+console.log('Main process', process.pid)
+
+setupMaster({ exec: `${__dirname}/web-app` })
+
+const cpuCount = require('os').cpus().length
+for (let i = 0; i < cpuCount; i++) {
+  const sub = fork()
+  console.log('Sub process', sub.process.pid)
+}
