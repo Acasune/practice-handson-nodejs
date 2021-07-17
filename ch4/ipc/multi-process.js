ccfa4056ba0dfb9ac7a5d31cb9becaa5058e3b44
Acasune <tttt4aaaa@gmail.com>
2021-07-17 15:26:02 +0900

    ch4:4-2-2

---
+++ ch4/ipc/multi-process.js
@@ -0,0 +1,15 @@
+﻿'use strict'
+const { fork, setupMaster } = require('cluster')
+
+console.log('Main process', process.pid)
+
+setupMaster({ exec: `${__dirname}/web-app` })
+
+const cpuCount = require('os').cpus().length
+for (let i = 0; i < cpuCount; i++) {
+  const sub = fork()
+  console.log('Sub process', sub.process.pid)
+  sub.send(3000)
+  sub.on('message', ({ pid, response }) =>
+    console.log(process.pid, `${pid} returns ${response}`))
+}
---
+++ ch4/ipc/web-app.js
@@ -0,0 +1,17 @@
+﻿'use strict'
+const http = require('http')
+const fibonacci = require('../fibonacci')
+const pid = process.pid
+
+process.on('message', port => {
+  console.log(pid, `Web serber is activate at ${port} port`)
+  http.createServer((req, res) => {
+    const n = Number(req.url.substr(1))
+    if (Number.isNaN(n)) {
+      return res.end()
+    }
+    const response = fibonacci(n)
+    process.send({ pid, response })
+    res.end(response.toString())
+  }).listen(port)
+})
