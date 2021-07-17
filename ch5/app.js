eac6f0c60ba48a525bca9690c7137fb403677782
Acasune <tttt4aaaa@gmail.com>
2021-07-17 19:13:04 +0900

    ch5:5-4-2 init

--- ch5/app.js
+++ ch5/app.js
@@ -6,6 +6,8 @@ let todos = [
 ]
 const app = express()
 
+app.use(express.json())
+
 app.get('/api/todos', (req, res) => {
   if (!req.query.completed) {
     return res.json(todos)
@@ -13,5 +15,25 @@ app.get('/api/todos', (req, res) => {
   const completed = req.query.completed === 'true'
   res.json(todos.filter(todo => todo.completed === completed))
 })
+
+let id = 2
+app.post('/api/todos', (req, res, next) => {
+  const { title } = req.body
+  if (typeof title !== 'string' || !title) {
+    const err = new Error('title is required')
+    err.statusCode = 400
+    return next(err)
+  }
+
+  const todo = { id: id += 1, title, completed: false }
+  todos.push(todo)
+  res.status(201).json(todo)
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  req.status(err.statusCode || 500).json({ error: err.message })
+})
+
 
 app.listen(3000)
